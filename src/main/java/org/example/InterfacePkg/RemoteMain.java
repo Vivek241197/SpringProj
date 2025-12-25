0e18303987c51e2690643f15d6b21c4ec8952b24
Vivek241197 <kvivak952@gmail.com>
2025-12-25 17:20:17 +0530

    Learning hashmap

--- src/main/java/org/example/InterfacePkg/RemoteMain.java
+++ src/main/java/org/example/InterfacePkg/RemoteMain.java
@@ -5,12 +5,23 @@ import java.util.*;
 public class RemoteMain {
 
     public static void main(String[] args) {
-        
-        System.out.println("true");
+        int[][] p = {{1,2,3,4},
+                    {3,0,7,8},
+                    {9,3,4,5}};
+        boolean[][] q = new boolean[p.length][p[0].length];
+        for(int i=0;i<p.length;i++){
+            for(int j=0;j<p[i].length;j++){
+                if(p[i][j]==0){
+                    q[i][j]=true;
+                }
+            }
+        }
+
+
+
     }
 }
 
-//
 class ListNode {
       int val;
       ListNode next;
