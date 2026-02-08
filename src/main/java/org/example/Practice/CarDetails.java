306ec1d6d4f52656a568fe44cfb2c6351e67fb39
Vivek241197 <126662005+Vivek241197@users.noreply.github.com>
2026-02-08 18:32:43 +0530

    Remove DFS implementation and add print statements

--- src/main/java/org/example/Practice/CarDetails.java
+++ src/main/java/org/example/Practice/CarDetails.java
@@ -6,39 +6,7 @@ import java.util.*;
 public class CarDetails {
 
     public static void main(String[] args) {
-//4,2,7,1,3,6,9
-        TreeNode root=new TreeNode(4);
-        root.left=new TreeNode(2);
-        root.right=new TreeNode(7);
-        root.left.left=new TreeNode(1);
-        root.left.right=new TreeNode(3);
-        root.right.left=new TreeNode(6);
-        root.right.right=new TreeNode(9);
-
-        dfs(root);
-
-    }
-    public static TreeNode dfs(TreeNode root) {
-        if(root == null) return null;
-        System.out.print(root.val+" ");
-       TreeNode temp=root.left;
-       root.left=root.right;
-       root.right=temp;
-        dfs(root.left);
-        dfs(root.right);
-        return root;
-    }
-}
-
-class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-     TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
+        System.out.printl("Doing something new");
+        System.ot.println("Ok bye");
       }
   }
